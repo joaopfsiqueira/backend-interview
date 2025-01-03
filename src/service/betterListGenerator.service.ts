31480790cc3793f31c149bdea5c89a69a6f456ff
joaopfsiqueira <joaopfsiqueiraa@gmail.com>
2025-01-03 17:21:00 -0300

    feat: service base

---
+++ src/service/betterListGenerator.service.ts
@@ -0,0 +1,6 @@
+import { IBetterListGeneratorService } from '../interfaces/service/betterListGenerator.service.interface';
+import { PatientModel } from '../model/patient.model';
+
+class BetterListGeneratorService implements IBetterListGeneratorService {}
+
+export default BetterListGeneratorService;
