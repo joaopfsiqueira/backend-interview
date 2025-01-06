ecfde076cd49d72acd2d2581417c48986cdeda4e
joaopfsiqueira <joaopfsiqueiraa@gmail.com>
2025-01-05 23:40:48 -0300

    feat: catching errors

--- src/service/betterListGenerator.service.ts
+++ src/service/betterListGenerator.service.ts
@@ -13,20 +13,24 @@ class BetterListGeneratorService implements IBetterListGeneratorService {
 	constructor(private calculator: ICalculations) {}
 
 	processList(reference: IGeolocation, debug: boolean): IResponsePatients[] | IResponseDetailsPatientsDebug[] {
-		// Load patients in memory
-		this.loadPatients();
+		try {
+			// Load patients in memory
+			this.loadPatients();
 
-		// i need to pass through patients twice. One to find the min and max values for each field and another to calculate the score
-		// Find min and max values for each field
-		const minMaxValues = this.calculator.minMaxFinder(this.patients, reference);
+			// i need to pass through patients twice. One to find the min and max values for each field and another to calculate the score
+			// Find min and max values for each field
+			const minMaxValues = this.calculator.minMaxFinder(this.patients, reference);
 
-		// Calculate the score for each patient using normalization and weight application
-		this.patients.forEach((patient) => {
-			this.calculator.patientScoreCalculator(patient, minMaxValues);
-		});
+			// Calculate the score for each patient using normalization and weight application
+			this.patients.forEach((patient) => {
+				this.calculator.patientScoreCalculator(patient, minMaxValues);
+			});
 
-		// Sort patients by score
-		return debug === false ? this.selectPatients() : this.selectPatientsDebug();
+			// Sort patients by score
+			return debug === false ? this.selectPatients() : this.selectPatientsDebug();
+		} catch (error) {
+			throw new Error(`Error processing list: ${error}`);
+		}
 	}
 
 	loadPatients(): void {
@@ -43,93 +47,101 @@ class BetterListGeneratorService implements IBetterListGeneratorService {
 	}
 
 	selectPatients(): IResponsePatients[] {
-		// separate patients by behavior
-		// select 8 best patients
-		const topPatients = this.patients
-			.filter((patient) => patient.score !== undefined && patient.behavior === true)
-			.sort((a, b) => (b.score ?? 0) - (a.score ?? 0))
-			.slice(0, process.env.TOTAL_TOP_PATIENTS)
-			.map((patient) => ({
-				id: patient.id,
-				name: patient.name,
-				score: patient.score!,
-				behavior: patient.behavior === true ? 'good' : 'little',
-			}));
+		try {
+			// separate patients by behavior
+			// select 8 best patients
+			const topPatients = this.patients
+				.filter((patient) => patient.score !== undefined && patient.behavior === true)
+				.sort((a, b) => (b.score ?? 0) - (a.score ?? 0))
+				.slice(0, process.env.TOTAL_TOP_PATIENTS)
+				.map((patient) => ({
+					id: patient.id,
+					name: patient.name,
+					score: patient.score!,
+					behavior: patient.behavior === true ? 'good' : 'little',
+				}));
 
-		// select 2 random patients with little behavior data
-		const selectedRandomPatients = this.patients
-			.filter((patient) => patient.behavior === false)
-			.sort(() => Math.random() - 0.5) // mix the array
-			.slice(0, process.env.TOTAL_RANDOM_PATIENTS)
-			.map((patient) => ({
-				id: patient.id,
-				name: patient.name,
-				score: patient.score!,
-				behavior: patient.behavior === true ? 'good' : 'little',
-			}));
+			// select 2 random patients with little behavior data
+			const selectedRandomPatients = this.patients
+				.filter((patient) => patient.behavior === false)
+				.sort(() => Math.random() - 0.5) // mix the array
+				.slice(0, process.env.TOTAL_RANDOM_PATIENTS)
+				.map((patient) => ({
+					id: patient.id,
+					name: patient.name,
+					score: patient.score!,
+					behavior: patient.behavior === true ? 'good' : 'little',
+				}));
 
-		// put everything together
-		const finalList = [...topPatients, ...selectedRandomPatients];
+			// put everything together
+			const finalList = [...topPatients, ...selectedRandomPatients];
 
-		return finalList;
+			return finalList;
+		} catch (error) {
+			throw new Error(`Error selecting patients: ${error}`);
+		}
 	}
 
 	selectPatientsDebug(): IResponseDetailsPatientsDebug[] {
-		// separate patients by behavior
-		// select 8 best patients
-		const topPatients = this.patients
-			.filter((patient) => patient.score !== undefined && patient.behavior === true)
-			.sort((a, b) => (b.score ?? 0) - (a.score ?? 0))
-			.slice(0, 8)
-			.map((patient) => ({
-				id: patient.id,
-				name: patient.name,
-				distance: patient.distance!,
-				age: patient.age,
-				acceptedOffers: patient.acceptedOffers,
-				canceledOffers: patient.canceledOffers,
-				averageReplyTime: patient.averageReplyTime,
-				score: {
-					age: patient.score!,
-					accepted: patient.acceptedOffers,
-					canceled: patient.canceledOffers,
-					reply: patient.averageReplyTime,
+		try {
+			// separate patients by behavior
+			// select 8 best patients
+			const topPatients = this.patients
+				.filter((patient) => patient.score !== undefined && patient.behavior === true)
+				.sort((a, b) => (b.score ?? 0) - (a.score ?? 0))
+				.slice(0, 8)
+				.map((patient) => ({
+					id: patient.id,
+					name: patient.name,
 					distance: patient.distance!,
-					total: patient.score!,
-				},
-				behaviorScore: patient.behaviorScore!,
-				behavior: patient.behavior === true ? 'good' : 'little',
-			}));
+					age: patient.age,
+					acceptedOffers: patient.acceptedOffers,
+					canceledOffers: patient.canceledOffers,
+					averageReplyTime: patient.averageReplyTime,
+					score: {
+						age: patient.score!,
+						accepted: patient.acceptedOffers,
+						canceled: patient.canceledOffers,
+						reply: patient.averageReplyTime,
+						distance: patient.distance!,
+						total: patient.score!,
+					},
+					behaviorScore: patient.behaviorScore!,
+					behavior: patient.behavior === true ? 'good' : 'little',
+				}));
 
-		// select 2 random patients with little behavior data
-		const selectedRandomPatients = this.patients
-			.filter((patient) => patient.behavior === false)
-			.sort(() => Math.random() - 0.5) // mix the array
-			.slice(0, 2)
-			.map((patient) => ({
-				id: patient.id,
-				name: patient.name,
-				distance: patient.distance!,
-				age: patient.age,
-				acceptedOffers: patient.acceptedOffers,
-				canceledOffers: patient.canceledOffers,
-				averageReplyTime: patient.averageReplyTime,
-				score: {
-					age: patient.score!,
-					accepted: patient.acceptedOffers,
-					canceled: patient.canceledOffers,
-					reply: patient.averageReplyTime,
+			// select 2 random patients with little behavior data
+			const selectedRandomPatients = this.patients
+				.filter((patient) => patient.behavior === false)
+				.sort(() => Math.random() - 0.5) // mix the array
+				.slice(0, 2)
+				.map((patient) => ({
+					id: patient.id,
+					name: patient.name,
 					distance: patient.distance!,
-					total: patient.score!,
-				},
-				behaviorScore: patient.behaviorScore!,
-				behavior: patient.behavior === true ? 'good' : 'little',
-			}));
+					age: patient.age,
+					acceptedOffers: patient.acceptedOffers,
+					canceledOffers: patient.canceledOffers,
+					averageReplyTime: patient.averageReplyTime,
+					score: {
+						age: patient.score!,
+						accepted: patient.acceptedOffers,
+						canceled: patient.canceledOffers,
+						reply: patient.averageReplyTime,
+						distance: patient.distance!,
+						total: patient.score!,
+					},
+					behaviorScore: patient.behaviorScore!,
+					behavior: patient.behavior === true ? 'good' : 'little',
+				}));
 
-		// put everything together
-		const finalList = [...topPatients, ...selectedRandomPatients];
+			// put everything together
+			const finalList = [...topPatients, ...selectedRandomPatients];
 
-		return finalList;
+			return finalList;
+		} catch (error) {
+			throw new Error(`Error selecting patients debug: ${error}`);
+		}
 	}
 }
 
