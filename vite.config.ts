0929b57afb662a026f9b403897d78c5441c1992d
joaopfsiqueira <joaopfsiqueiraa@gmail.com>
2025-01-06 04:10:17 -0300

    feat: supertest to increase controller test and some changes in vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,25 @@
 import { defineConfig } from 'vitest/config';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 export default defineConfig({
 	test: {
+		mockReset: true,
+		globals: true,
+		environment: 'node',
 		coverage: {
+			exclude: [
+				'**/node_modules/**',
+				'**/src/model/**',
+				'**/dist/**',
+				'**/src/utils/interfaces/**',
+				'**/src/utils/types/**',
+				'**/vite.config.ts',
+				'**/src/environment.d.ts',
+				'**/src/app.ts',
+				'**/src/server.ts',
+			],
 			branches: 80,
 			functions: 80,
 			lines: 80,
